17597aa20d5ebad488142d4cfa72b0ef584d6bf0
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2021-03-09 14:12:54 -0500

    Create methods for ICommand class
    Also create methods for MoveCommand class which implements the ICommand interface.

--- janggi/core/helpers/command.py
+++ janggi/core/helpers/command.py
@@ -1,6 +1,57 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from utils.point import Point2D
+    from janggi_board import JanggiBoard
+
+
 class ICommand:
-    pass
+    """Interface representing Command class for implementing the Command Pattern."""
+
+    def execute(self) -> None:
+        """Execute a command."""
+        pass
+
+    def un_execute(self) -> None:
+        """Execute the inverse of a command."""
+        pass
 
 
 class MoveCommand(ICommand):
-    pass
+    """Store the state of a move request made by a JanggiGame instance."""
+
+    def __init__(self, source: Point2D, destination: Point2D, board: JanggiBoard) -> None:
+        """
+        Create new instance of a move command and save the source, destination, and board state.
+
+        :param source: Source coordinate.
+        :param destination: Destination coordinate.
+        :param board: Game board.
+        """
+
+        self._source: Point2D = source
+        self._destination: Point2D = destination
+        self._board: JanggiBoard = board
+
+    @property
+    def source(self) -> Point2D:
+        return self._source
+
+    @property
+    def destination(self) -> Point2D:
+        return self._destination
+
+    @property
+    def board(self) -> JanggiBoard:
+        return self._board
+
+    def execute(self) -> None:
+        """Call move on the board object to signify a move from source to destination."""
+
+        self.board.move(self.source, self.destination)
+
+    def un_execute(self) -> None:
+        """Call move on the board object to signify a move from destination to source."""
+
+        self.board.move(self.destination, self.source)
