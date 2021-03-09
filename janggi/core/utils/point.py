9a7b00da3e6a11a050564210913db7043de26bd9
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2021-03-09 17:21:49 -0500

    Create methods for Point2D class

--- janggi/core/utils/point.py
+++ janggi/core/utils/point.py
@@ -1,2 +1,48 @@
+from __future__ import annotations
+
+from typing import Union
+
+
 class Point2D:
-    pass
+    """A class representing a 2D point in space."""
+
+    def __init__(self, x: Union[int, float], y: Union[int, float]) -> None:
+        """
+        Initialize a Point2D object with x and y coordinates.
+
+        :param x: The x coordinate of the point.
+        :param y: The y coordinate of the point.
+        """
+
+        self._x = x
+        self._y = y
+
+    @property
+    def x(self) -> Union[int, float]:
+        return self._x
+
+    @property
+    def y(self) -> Union[int, float]:
+        return self._y
+
+    def __add__(self, other: Point2D):
+        """Override addition operator to assist in performing vector addition."""
+
+        return Point2D(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: Point2D):
+        """Override subtraction operator to assist in performing vector subtraction."""
+
+        return Point2D(self.x - other.x, self.y - other.y)
+
+    def __eq__(self, other: Point2D):
+        """Override the equality operator to assist in comparing two points by their x andy attributes."""
+
+        return (self.x, self.y) == (other.x, other.y)
+
+    def __ne__(self, other: Point2D):
+        """Override the inequality operator to assist in comparing two points by they x and y attributes."""
+        return (self.x, self.y) != (other.x, other.y)
+
+    def to_tuple(self):
+        return self.x, self.y
