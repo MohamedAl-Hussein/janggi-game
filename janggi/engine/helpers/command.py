32d86ff1f304c48f9f83ac5f48960988f350b784
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2021-03-18 17:07:16 -0400

    Refactor MoveCommand class
    Change properties from protected to private. Add additional properties and checks to track removed pieces to aid in reversing moves.

--- janggi/engine/helpers/command.py
+++ janggi/engine/helpers/command.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
 import abc
-from typing import TYPE_CHECKING
+from typing import Optional, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from utils.point import Point2D
     from janggi_board import JanggiBoard
+    from janggi_game import GameState
+    from janggi_piece import JanggiPiece
 
 
 class ICommand(metaclass=abc.ABCMeta):
@@ -31,37 +33,65 @@ class ICommand(metaclass=abc.ABCMeta):
 class MoveCommand(ICommand):
     """Store the state of a move request made by a JanggiGame instance."""
 
-    def __init__(self, source: Point2D, destination: Point2D, board: JanggiBoard) -> None:
+    def __init__(self, source: Point2D, destination: Point2D, board: JanggiBoard, game_state: GameState) -> None:
         """
-        Create new instance of a move command and save the source, destination, and board state.
+        Create new instance of object and save source, destination, and board state.
 
         :param source: Source coordinate.
         :param destination: Destination coordinate.
-        :param board: Game board.
+        :param board: Game board map.
         """
 
-        self._source: Point2D = source
-        self._destination: Point2D = destination
-        self._board: JanggiBoard = board
+        self.__source: Point2D = source
+        self.__destination: Point2D = destination
+        self.__board: JanggiBoard = board
+        self.__game_state: GameState = game_state
+        self.__removed_piece: Optional[JanggiPiece] = None
 
     @property
     def source(self) -> Point2D:
-        return self._source
+        return self.__source
 
     @property
     def destination(self) -> Point2D:
-        return self._destination
+        return self.__destination
 
     @property
     def board(self) -> JanggiBoard:
-        return self._board
+        return self.__board
+
+    @property
+    def game_state(self) -> GameState:
+        return self.__game_state
+
+    @game_state.setter
+    def game_state(self, value: GameState) -> None:
+        self.__game_state = value
+
+    @property
+    def removed_piece(self) -> Optional[JanggiPiece]:
+        """Return the piece that was removed as a result of the move command."""
+
+        return self.__removed_piece
+
+    @removed_piece.setter
+    def removed_piece(self, value: Optional[JanggiPiece]) -> None:
+        """Set the removed piece to the object that is going to be removed."""
+
+        self.__removed_piece = value
 
     def execute(self) -> None:
-        """Call move on the board object to signify a move from source to destination."""
+        """Move object from source to destination."""
 
+        self.removed_piece = self.board.coord_map.get(self.destination.to_tuple())
         self.board.move(self.source, self.destination)
 
     def un_execute(self) -> None:
-        """Call move on the board object to signify a move from destination to source."""
+        """Move object from destination to source."""
 
         self.board.move(self.destination, self.source)
+
+        # Return removed piece back to its original location (if one existed).
+        if self.removed_piece is not None:
+            self.board.coord_map[self.destination.to_tuple()] = self.removed_piece
+            self.removed_piece = None
