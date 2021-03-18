6140f99b58fc7251613a8f20d8101cf00d388732
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2021-03-18 17:08:51 -0400

    Refactor CommandManager class
    Change properties from protected to private. Add last_command property to aid in getting/setting game state for final command.

--- janggi/engine/helpers/command_manager.py
+++ janggi/engine/helpers/command_manager.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import Optional,TYPE_CHECKING
 
 if TYPE_CHECKING:
     from command import ICommand
@@ -11,10 +11,24 @@ class CommandManager:
     """Responsible for doing, undoing, and redoing commands."""
 
     def __init__(self, undo_stack: Stack, redo_stack: Stack) -> None:
-        """Create a new undo_stack and redo_stack to store undo commands and redo commands respectively."""
+        """
+        Create a new undo_stack and redo_stack to store undo commands and redo commands respectively.
 
-        self._undo_stack: Stack = undo_stack
-        self._redo_stack: Stack = redo_stack
+        :param undo_stack: Instance of stack for storing undo commands.
+        :param redo_stack: Instance of stack for storing redo commands.
+        """
+
+        self.__undo_stack: Stack = undo_stack
+        self.__redo_stack: Stack = redo_stack
+        self.__last_command: Optional[ICommand] = None
+
+    @property
+    def last_command(self) -> Optional[ICommand]:
+        return self.__last_command
+
+    @last_command.setter
+    def last_command(self, value: ICommand) -> None:
+        self.__last_command = value
 
     def do(self, command: ICommand) -> None:
         """
@@ -26,25 +40,31 @@ class CommandManager:
         """
 
         command.execute()
-        self._undo_stack.push(command)
-        self._redo_stack.flush()
+        self.__undo_stack.push(command)
+        self.__redo_stack.flush()
+
+        self.last_command = command
 
     def undo(self) -> None:
         """Pop command off undo_stack and call its un_execute method. Then push it onto the redo stack."""
 
-        if self._undo_stack.is_empty():
+        if self.__undo_stack.is_empty():
             return
 
-        command = self._undo_stack.pop()
-        self._redo_stack.push(command)
+        command = self.__undo_stack.pop()
+        self.__redo_stack.push(command)
         command.un_execute()
+
+        self.last_command = command
 
     def redo(self) -> None:
         """Pop command off redo_stack and call its execute method. Then push it onto the undo_stack."""
 
-        if self._redo_stack.is_empty():
+        if self.__redo_stack.is_empty():
             return
 
-        command = self._redo_stack.pop()
-        self._undo_stack.push(command)
+        command = self.__redo_stack.pop()
+        self.__undo_stack.push(command)
         command.execute()
+
+        self.last_command = command
