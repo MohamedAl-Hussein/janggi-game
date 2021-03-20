b170c5d772746d47af7ff42fc16b78c31fbcb259
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2021-03-20 13:11:52 -0400

    Add destination property to Piece class
    Use to hold a collection of destination coordinates a piece can visit.

--- janggi/game/Game/Piece/Piece.cs
+++ janggi/game/Game/Piece/Piece.cs
@@ -1,9 +1,11 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class Piece : Area2D 
 {
     public Tile OccupyingTile;
+    public List<Tuple<int, int>> Destinations = new List<Tuple<int, int>>(); 
 
     [Signal]
     public delegate void PieceSelected(Piece piece);
