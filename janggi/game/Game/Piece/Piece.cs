01c13e40a312c51f76e084f0cd5d8f22502985e7
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2021-03-20 14:19:38 -0400

    Add functionality for changing turns after a move
    Track color of current player and only allow moves for that player. Update to next player after a successful move is made.

--- janggi/game/Game/Piece/Piece.cs
+++ janggi/game/Game/Piece/Piece.cs
@@ -1,11 +1,13 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using janggi.Game.Piece;
 
 public class Piece : Area2D 
 {
     public Tile OccupyingTile;
-    public List<Tuple<int, int>> Destinations = new List<Tuple<int, int>>(); 
+    public List<Tuple<int, int>> Destinations = new List<Tuple<int, int>>();
+    public PieceColor Color;
 
     [Signal]
     public delegate void PieceSelected(Piece piece);
