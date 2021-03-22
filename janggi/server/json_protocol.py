58b232f439c74a126ca07e07bcfbcd96ef2b08d5
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2021-03-22 17:10:12 -0400

    Create Python server demo
    Simple echo server using asyncio that modifies the message before sending it back. Use to test communication between C# client and Python server.

---
+++ janggi/server/json_protocol.py
@@ -0,0 +1,13 @@
+import json
+
+from protocol import Protocol
+
+
+class JsonMessageProtocol(Protocol):
+    def encode_body(self, message):
+        # Convert message to byte array
+        return str(json.dumps(message)).encode('utf-8')
+
+    def decode(self, message):
+        # Convert bytes read to json
+        return json.loads(message)
