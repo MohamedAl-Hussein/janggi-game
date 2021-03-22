c22893416b0fe003380a76a1811f88a9b483e0fe
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2021-03-22 17:08:53 -0400

    Create C# client demo
    Code sourced from TCP tutorial. Use as a baseline to get a C# client setup to communicate with Python server.

---
+++ janggi/client/Client/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Shared;
+
+namespace Client
+{
+
+    public class MyMessage
+    {
+        public string StringProperty { get; set; }
+        public int IntProperty { get; set; }
+    }
+
+
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Console.WriteLine("Press Enter to Connect");
+            Console.ReadLine();
+
+            var endpoint = new IPEndPoint(IPAddress.Loopback, 9001);
+
+            var channel = new ClientChannel<JsonMessageProtocol,JObject>();
+
+            channel.OnMessage(OnMessage);
+
+            await channel.ConnectAsync(endpoint).ConfigureAwait(false);
+
+            var myMessage = new MyMessage
+            {
+                IntProperty = 404,
+                StringProperty = "Hello World"
+            };
+
+            Console.WriteLine("Sending");
+            Print(myMessage);
+
+            await channel.SendAsync(myMessage).ConfigureAwait(false);
+
+            Console.ReadLine();
+
+            await channel.ConnectAsync(endpoint).ConfigureAwait(false);
+            Console.WriteLine("Sending");
+            Print(myMessage);
+            await channel.SendAsync(myMessage).ConfigureAwait(false);
+
+            Console.ReadLine();
+        }
+
+        static Task OnMessage(JObject jObject)
+        {
+            Console.WriteLine("Received JObject Message");
+            Print(Convert(jObject));
+            return Task.CompletedTask;
+        }
+
+        static MyMessage Convert(JObject jObject)
+            => jObject.ToObject(typeof(MyMessage)) as MyMessage;
+
+        static void Print(MyMessage m) => Console.WriteLine($"MyMessage.IntProperty = {m.IntProperty}, MyMessage.StringProperty = {m.StringProperty}");
+    }
+}
