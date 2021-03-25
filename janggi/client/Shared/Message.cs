41441c6f00ec4b4b7ef727faaa696d1dbd9d461e
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2021-03-24 21:21:11 -0400

    Create new Message class for client

---
+++ janggi/client/Shared/Message.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+
+namespace Shared
+{
+    public class Message
+    {
+        public MessageAction Action { get; set; }
+
+        public MessageData Data { get; set; }
+    }
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum MessageAction
+    {
+        NEW_GAME,
+        GAME_STARTED,
+        SETUP_COMPLETED,
+        SETUP_CONFIRMED,
+        GET_GAME_STATUS,
+        GAME_STATUS,
+        GET_PIECE_DESTINATIONS,
+        PIECE_DESTINATIONS,
+        MOVE_COMPLETED,
+        MOVE_CONFIRMED,
+        END_GAME,
+        GAME_OVER,
+        DEFAULT 
+    }
+    public class MessageData { }
+
+    public class PieceData : MessageData
+    {
+        public List<PieceDTO> Pieces { get; set; }
+    }
+
+    public class InitialSetup : MessageData 
+    { 
+        public bool BlueLeftTransposed { get; set; }
+        public bool BlueRightTransposed { get; set; }
+        public bool RedLeftTransposed { get; set; }
+        public bool RedRightTransposed { get; set; }
+    }
+
+    public class GameStatus : MessageData 
+    { 
+        public string GameState { get; set; }
+        public string PlayerTurn { get; set; }
+        public bool IsChecked { get; set; }
+    }
+
+    public class PieceDestinations : MessageData 
+    { 
+        public List<int> Source { get; set; }
+        public List<List<int>> Destinations { get; set; }
+    }
+
+    public class MoveRequest : MessageData 
+    { 
+        public Tuple<int, int> Source { get; set; }
+        public Tuple<int, int> Destination { get; set; }
+    }
+}
