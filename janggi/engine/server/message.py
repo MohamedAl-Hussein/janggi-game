c0cec4702f9c3bae63f5c6d0cbc7e16eff9f6d27
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2021-03-24 21:18:02 -0400

    Rename MessageDTO class and its subclasses' properties
    New name is Message, and properties have their first letter capitalized to ease in translation between C# client and Python server.

--- janggi/engine/server/message.py
+++ janggi/engine/server/message.py
@@ -1,27 +1,66 @@
+from __future__ import annotations
+
 import dataclasses
+from enum import auto, Enum
+from typing import List
+
+from server.piece_dto import PieceDTO
 
 
 @dataclasses.dataclass
-class MessageDTO:
+class Message:
+    Action: MessageAction
+    Data: MessageData
+
+
+class MessageAction(Enum):
+    NEW_GAME = auto()
+    GAME_STARTED = auto()
+    SETUP_COMPLETED = auto()
+    SETUP_CONFIRMED = auto()
+    GET_GAME_STATUS = auto()
+    GAME_STATUS = auto()
+    GET_PIECE_DESTINATIONS = auto()
+    PIECE_DESTINATIONS = auto()
+    MOVE_COMPLETED = auto()
+    MOVE_CONFIRMED = auto()
+    END_GAME = auto()
+    GAME_OVER = auto()
+    DEFAULT = auto()
+
+
+@dataclasses.dataclass
+class MessageData:
     pass
 
 
-class GameStateMessageDTO(MessageDTO):
-    player_turn: str
-    game_state: str
+@dataclasses.dataclass
+class GameStatus(MessageData):
+    GameState: str
+    PlayerTurn: str
+    IsChecked: bool
 
 
-class DestinationMessageDTO(MessageDTO):
-    coordinate: (int, int)
+@dataclasses.dataclass
+class PieceDestinations(MessageData):
+    Source: List[int]
+    Destinations: List[List[int]]
 
 
-class StartLayoutMessageDTO(MessageDTO):
-    l_blue_swap: bool
-    r_blue_swap: bool
-    l_red_swap: bool
-    r_red_swap: bool
+@dataclasses.dataclass
+class SetupCompleted(MessageData):
+    BlueLeftTransposed: bool
+    BlueRightTransposed: bool
+    RedLeftTransposed: bool
+    RedRightTransposed: bool
 
 
-class MoveRequestDTO(MessageDTO):
-    source: (int, int)
-    destination: (int, int)
+@dataclasses.dataclass
+class MoveCompleted(MessageData):
+    Source: List[int]
+    Destination: List[int]
+
+
+@dataclasses.dataclass
+class PieceData(MessageData):
+    Pieces: List[PieceDTO]
