ca41ff70322e1464d40f6feb08f18e06e75d0879
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2021-03-24 21:20:04 -0400

    Complete ActionRequestHandler's handle_request method
    Method renamed to create_response. 
    Method now takes care of updating JanggiGame instance and returning requested data.

--- janggi/engine/server/action_request.py
+++ janggi/engine/server/action_request.py
@@ -1,38 +1,50 @@
-import enum
+from typing import List
 
-# from ..engine.janggi_game import JanggiGame
+from engine.core.janggi_game import JanggiGame
+from server.message import Message, MessageData, MessageAction, GameStatus, PieceDestinations, PieceDTO, PieceData
 
 
 class ActionRequestHandler:
-    def __init__(self, action, message):
-        self.action = action
+    def __init__(self, message, server):
         self.message = message
+        self.server = server
 
-    def handle_request(self):
-        if self.action is ActionRequest.INVALID_REQUEST:
-            raise
+    def create_response(self):
+        # create a response based on message content
+        response = Message(MessageAction.DEFAULT, MessageData())
 
-        if self.action == ActionRequest.NEW_GAME:
-            pass
-            # return JanggiGame()
-        elif self.action == ActionRequest.UPDATE_START_LAYOUT:
-            pass
-        elif self.action == ActionRequest.GET_GAME_STATE:
-            pass
-        elif self.action == ActionRequest.GET_DESTINATIONS:
-            pass
-        elif self.action == ActionRequest.PROCESS_MOVE:
-            pass
-        elif self.action == ActionRequest.END_GAME:
+        if self.message.Action is MessageAction.DEFAULT:
             pass
 
+        if self.message.Action is MessageAction.NEW_GAME:
+            self.server.game = JanggiGame()
 
+            pieces: List[PieceDTO] = list()
+            for position, piece in self.server.game.board.coord_map.items():
+                dto: PieceDTO = PieceDTO(
+                    Position=list(position),
+                    Color=piece.color.name,
+                    Category=piece.category.name
+                )
 
-class ActionRequest(enum.Enum):
-    NEW_GAME = enum.auto()
-    UPDATE_START_LAYOUT = enum.auto()
-    GET_GAME_STATE = enum.auto()
-    GET_DESTINATIONS = enum.auto()
-    PROCESS_MOVE = enum.auto()
-    END_GAME = enum.auto()
-    INVALID_REQUEST = enum.auto()
+                pieces.append(dto)
+            response = Message(MessageAction.GAME_STARTED, PieceData(pieces))
+        elif self.message.Action is MessageAction.END_GAME:
+            self.server.game = None
+            response = Message(MessageAction.GAME_OVER, MessageData())
+        elif self.message.Action is MessageAction.GET_GAME_STATUS:
+            response = Message(MessageAction.GAME_STATUS, GameStatus(**self.server.game.return_game_status()))
+        elif self.message.Action is MessageAction.SETUP_COMPLETED:
+            self.server.game.transpose_pieces(self.message.Data.__dict__)
+            response = Message(MessageAction.SETUP_CONFIRMED, MessageData())
+        elif self.message.Action is MessageAction.MOVE_COMPLETED:
+            self.server.game.perform_move_using_tuple_coords(self.message.Data.Source, self.message.Data.Destination)
+            response = Message(MessageAction.MOVE_CONFIRMED, MessageData())
+        elif self.message.Action is MessageAction.GET_PIECE_DESTINATIONS:
+            destinations = self.server.game.return_piece_destinations(self.message.Data.Source)
+            response = Message(
+                MessageAction.PIECE_DESTINATIONS,
+                PieceDestinations(Source=self.message.Data.Source, Destinations=destinations)
+            )
+
+        return response
