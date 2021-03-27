829240989ce8bf81d802b5c04f1a2ae538435fb4
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2021-03-27 18:51:19 -0400

    Refactor Board and Game classes
    Move game-related methods to Game class to simplify the Board's responsibilities. 
    Add new handlers to Game class to send/receive data using the Client class. 
    Split larger code chunks into sub-methods.
    Add converters for various coordinate translations in game.

--- JanggiGame/Game/Game/Board/Board.cs
+++ JanggiGame/Game/Game/Board/Board.cs
@@ -1,198 +1,108 @@
+using Common;
 using Godot;
-using janggi.Game.Piece;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 
 public class Board : Node2D
 {
-    [Signal]
-    public delegate void MoveCompleted();
+    private const string TILE_SCENE = "res://Game/Tile/Tile.tscn";
+    private const string PIECE_SCENE = "res://Game/Piece/Piece.tscn";
+    private const string PIECE_ASSET_PATH = "res://Assets/Pieces/"; 
+    private const int GRID_COLS = 9;
+    private const int GRID_ROWS = 10;
+    private const int TILE_OFFSET = 18;
+    private const int PIECE_OFFSET = 32;
+    private const int POSITION_OFFSET = TILE_OFFSET + PIECE_OFFSET;
+    private const int SCALE = 100;
 
-    private readonly int _tileOffset = 18;
-    private readonly int _pieceOffset = 32;
-    private readonly int _scale = 100;
-    public Tile Source;
-    public Tile Destination;
-    public PieceColor Turn = PieceColor.BLUE;
-    private readonly PieceData[] _pieceData = new PieceData[]
-    {
-        new PieceData(0, "red", "chariot", (x : 0, y : 0)),
-        new PieceData(1, "red", "elephant", (x : 1, y : 0)),
-        new PieceData(2, "red", "horse", (x : 2, y : 0)),
-        new PieceData(3, "red", "guard", (x : 3, y : 0)),
-        new PieceData(4, "red", "guard", (x : 5, y : 0)),
-        new PieceData(5, "red", "elephant", (x : 6, y : 0)),
-        new PieceData(6, "red", "horse", (x : 7, y : 0)),
-        new PieceData(7, "red", "chariot", (x : 8, y : 0)),
-        new PieceData(8, "red", "general", (x : 4, y : 1)),
-        new PieceData(9, "red", "cannon", (x : 1, y : 2)),
-        new PieceData(10, "red", "cannon", (x : 7, y : 2)),
-        new PieceData(11, "red", "soldier", (x : 0, y : 3)),
-        new PieceData(12, "red", "soldier", (x : 2, y : 3)),
-        new PieceData(13, "red", "soldier", (x : 4, y : 3)),
-        new PieceData(14, "red", "soldier", (x : 6, y : 3)),
-        new PieceData(15, "red", "soldier", (x : 8, y : 3)),
-        new PieceData(16, "blue", "chariot", (x : 0, y : 9)),
-        new PieceData(17, "blue", "elephant", (x : 1, y : 9)),
-        new PieceData(18, "blue", "horse", (x : 2, y : 9)),
-        new PieceData(19, "blue", "guard", (x : 3, y : 9)),
-        new PieceData(20, "blue", "guard", (x : 5, y : 9)),
-        new PieceData(21, "blue", "elephant", (x : 6, y : 9)),
-        new PieceData(22, "blue", "horse", (x : 7, y : 9)),
-        new PieceData(23, "blue", "chariot", (x : 8, y : 9)),
-        new PieceData(24, "blue", "general", (x : 4, y : 8)),
-        new PieceData(25, "blue", "cannon", (x : 1, y : 7)),
-        new PieceData(26, "blue", "cannon", (x : 7, y : 7)),
-        new PieceData(27, "blue", "soldier", (x : 0, y : 6)),
-        new PieceData(28, "blue", "soldier", (x : 2, y : 6)),
-        new PieceData(29, "blue", "soldier", (x : 4, y : 6)),
-        new PieceData(30, "blue", "soldier", (x : 6, y : 6)),
-        new PieceData(31, "blue", "soldier", (x : 8, y : 6))
-    };
-    private Dictionary<Tuple<int, int>, Node> _tileMap = new Dictionary<Tuple<int, int>, Node>();
+
+    public Dictionary<Coordinate, Tile> TileMap = new Dictionary<Coordinate, Tile>();
 
     public override void _Ready()
-    {
-        SetupTiles();
-        SetupPieces();
-    }
+        => SetupTiles();
 
     // Create new tiles and place them on every intersection on the board.
     public void SetupTiles()
     {
-        var tile = GD.Load<PackedScene>("res://Game/Tile/Tile.tscn");
-        for (int x = 0; x < 9; x++)
+        var tile = GD.Load<PackedScene>(TILE_SCENE);
+        for (int x = 0; x < GRID_COLS; x++)
         {
-            for (int y = 0; y < 10; y++)
+            for (int y = 0; y < GRID_ROWS; y++)
             {
                 Tile newTile = (Tile)tile.Instance();
-                newTile.Coordinates = new Tuple<int, int>(x, y);
-                newTile.Position = new Vector2(x * this._scale + this._tileOffset, y * this._scale + this._tileOffset);
-                newTile.Connect("TileSelected", this, "OnTileSelected");
+                newTile.Coordinates = new Coordinate { x_coord = x, y_coord = y };
+                newTile.Position = new Vector2(x * SCALE + TILE_OFFSET, y * SCALE + TILE_OFFSET);
+                newTile.Connect("TileSelected", GetParent(), "OnTileSelected");
                 AddChild(newTile);
 
-                _tileMap.Add(new Tuple<int, int>(x, y), newTile);
+                TileMap.Add(newTile.Coordinates, newTile);
             }
         }
     }
-    public void SetupPieces()
+    public void SetupPieces(List<PieceDTO> pieceDTOs)
     {
-        var piece = GD.Load<PackedScene>("res://Game/Piece/Piece.tscn");
+        var piece = GD.Load<PackedScene>(PIECE_SCENE);
 
-        int offset = this._tileOffset + this._pieceOffset;
-        foreach (PieceData data in _pieceData)
+        foreach(PieceDTO dto in pieceDTOs)
         {
+            // Create Piece
             Piece newPiece = (Piece)piece.Instance();
-            if (data.Color == "red")
-            {
-                newPiece.Color = PieceColor.RED;
-            }
-            else
-            {
-                newPiece.Color = PieceColor.BLUE;
-            }
-
-            Texture texture = (Texture)ResourceLoader.Load($"res://Assets/Pieces/HanjaBlue/{data.Color}_{data.Category}.png");
+            newPiece.FromDTO(dto);
+            Texture texture = (Texture)ResourceLoader.Load(PieceAssetPath(newPiece));
             newPiece.GetNode<Sprite>("Sprite").Texture = texture;
-            newPiece.Position = new Vector2(data.Position.x * this._scale + offset, data.Position.y * this._scale + offset);
-            newPiece.Destinations.Add(new Tuple<int, int>(data.Position.x + 1, data.Position.y));
+            newPiece.Position = ToPosition(newPiece.Coordinates);
+
+            // Add to Board
             AddChild(newPiece);
 
-            Tuple<int, int> key = new Tuple<int, int>(data.Position.x, data.Position.y);
-
-            Tile tile = (Tile)_tileMap[key];
-            tile.Occupant = (Piece)newPiece;
+            // Add to tile map
+            Coordinate key = new Coordinate 
+            { 
+                x_coord = newPiece.Coordinates.x_coord, 
+                y_coord = GRID_ROWS - 1 - newPiece.Coordinates.y_coord 
+            };
+            Tile tile = TileMap[key];
+            tile.Occupant = newPiece;
         }
     }
+    private string PieceAssetPath(Piece piece)
+        => $"{PIECE_ASSET_PATH}HanjaBlue/{piece.Color.ToString().ToLower()}_{piece.Category.ToString().ToLower()}.png";
 
-    public void OnTileSelected(Tile tile)
+    #region Converters
+
+    public Coordinate FromPosition(Vector2 position)
     {
-        GD.Print($"TILE selected at position: {tile.Position}");
-        GD.Print($"OCCUPANT is: {tile.Occupant?.GetType()}");
+        float x = (position.x - POSITION_OFFSET) / SCALE;
+        float y = -((position.y - POSITION_OFFSET) / SCALE - GRID_ROWS + 1);
 
-        // No piece at source and tile selected contains piece.
-        if (this.Source is null && !(tile.Occupant is null))
-        {
-            // Only highlight if it is the color's turn.
-            if (tile.Occupant.Color == Turn)
-            {
-                this.Source = tile;
-                tile.HighlightTile();
-                foreach (Tuple<int, int> destination in tile.Occupant.Destinations)
-                {
-                    if (!_tileMap.ContainsKey(destination))
-                    {
-                        return;
-                    }
-                    Tile destTile = (Tile)_tileMap[destination];
-
-                    destTile.HighlightTile();
-                    GD.Print($"DESTINATION: {destination}.");
-                }
-            }
-            return;
-        }
-
-        // Piece at source so tile selected is destination.
-        if (!(this.Source is null))
-        {
-            this.Destination = tile;
-
-            GD.Print($"MOVE from {this.Source.Occupant.Position} to {this.Destination.Position}");
-            MovePiece();
-
-            // Clear all tile destination highlights.
-            this.Source.UnHighlightTile();
-
-            foreach (Tile t in _tileMap.Values)
-            {
-                t.UnHighlightTile();
-            }
-
-            // Clear source and destination.
-            this.Source = null;
-            this.Destination = null;
-        }
+        return new Coordinate { x_coord = (int)x, y_coord = (int)y };
     }
 
-    public void MovePiece()
+    public Vector2 ToPosition(Coordinate location)
     {
-        if (this.Source.Occupant == this.Destination.Occupant)
-        {
-            return;
-        }
+        int x = location.x_coord * SCALE + POSITION_OFFSET;
+        int y = (-location.y_coord + GRID_ROWS - 1) * SCALE + POSITION_OFFSET;
 
-        // Check if destination can be reached by piece.
-        bool isDestinationReachable = this.Source.Occupant.Destinations
-            .Where(dest => dest.Item1 == this.Destination.Coordinates.Item1 && dest.Item2 == this.Destination.Coordinates.Item2)
-            .Any();
-
-        if (!isDestinationReachable)
-        {
-            return;
-        }
-
-        // Move to destination.
-        Piece target = this.Destination.Occupant;
-
-        if (!(target is null))
-        {
-            target.QueueFree();
-        }
-
-        this.Destination.Occupant = this.Source.Occupant;
-        this.Source.Occupant = null;
-
-        Vector2 destination = this.Destination.Position;
-        destination.x += this._pieceOffset;
-        destination.y += this._pieceOffset;
-
-        this.Destination.Occupant.Position = destination;
-        this.EmitSignal("MoveCompleted");
+        return new Vector2(x, y);
     }
 
-    public override void _Process(float delta)
+    public Vector2 NewDestination(Vector2 oldDestination)
     {
+        int newDestX = (int)(oldDestination.x + PIECE_OFFSET);
+        int newDestY = (int)(oldDestination.y + PIECE_OFFSET);
+
+        return new Vector2(newDestX, newDestY);
     }
+
+    public Coordinate ConvertYCoord(Coordinate position)
+        => new Coordinate { x_coord = position.x_coord, y_coord = GRID_COLS - position.y_coord };
+
+    public Coordinate FromTilePosition(Vector2 position)
+    {
+        int x = (int)(position.x - TILE_OFFSET) / SCALE;
+        int y = (int)(position.y - TILE_OFFSET) / SCALE;
+
+        return new Coordinate { x_coord = x, y_coord = y };
+    }
+
+    #endregion
 }
