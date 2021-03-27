0ea31d4c5845c40f7788fd3f3382d211810f9bd1
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2021-03-27 18:44:29 -0400

    Split PieceDTO class
    Class split into PieceDTO, PieceColor, and PieceCategory classes.

---
+++ JanggiGame/Common/PieceCategory.cs
@@ -0,0 +1,13 @@
+﻿namespace Common
+{
+    public enum PieceCategory
+    {
+        GENERAL,
+        GUARD, 
+        HORSE, 
+        ELEPHANT,
+        CHARIOT,
+        CANNON,
+        SOLDIER
+    }
+}
