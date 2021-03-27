1115a9680f8fbef82f53d1a61071c60facca758a
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2021-03-27 18:27:22 -0400

    Create new packages for SocketServer
    Packages will be used to split logical components of server.

---
+++ JanggiGame/SocketServer/messages/__init__.py
@@ -0,0 +1,4 @@
+from .message import Message
+from .message_action import MessageAction
+from .message_data import MessageData, SetupCompleted, PieceDestinations, MoveCompleted, GameStatus, PieceData
+from .message_serialization import MessageDecoder, MessageEncoder
---
+++ JanggiGame/SocketServer/protocols/__init__.py
@@ -0,0 +1,2 @@
+from .json_protocol import JsonMessageProtocol
+from .protocol import Protocol
