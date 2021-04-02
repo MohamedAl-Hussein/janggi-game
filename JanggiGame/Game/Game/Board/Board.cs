49e3de1108394ad16b1b1ad1ec2ada0cdf4e08c4
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2021-04-02 15:24:17 -0400

    Split Board class's offset constants to x and y
    Makes it easier to adjust tile and piece offsets when board position changes.
    
    Also update offsets to reflect game window resizing and padding.

--- JanggiGame/Game/Game/Board/Board.cs
+++ JanggiGame/Game/Game/Board/Board.cs
@@ -9,9 +9,11 @@ public class Board : Node2D
     private const string PIECE_ASSET_PATH = "res://Assets/Pieces/"; 
     private const int GRID_COLS = 9;
     private const int GRID_ROWS = 10;
-    private const int TILE_OFFSET = 18;
+    private const int TILE_OFFSET_X = 104;
+    private const int TILE_OFFSET_Y = 52;
     private const int PIECE_OFFSET = 32;
-    private const int POSITION_OFFSET = TILE_OFFSET + PIECE_OFFSET;
+    private const int POSITION_OFFSET_X = TILE_OFFSET_X + PIECE_OFFSET;
+    private const int POSITION_OFFSET_Y = TILE_OFFSET_Y + PIECE_OFFSET;
     private const int SCALE = 100;
 
 
@@ -30,7 +32,7 @@ public class Board : Node2D
             {
                 Tile newTile = (Tile)tile.Instance();
                 newTile.Coordinates = new Coordinate { x_coord = x, y_coord = y };
-                newTile.Position = new Vector2(x * SCALE + TILE_OFFSET, y * SCALE + TILE_OFFSET);
+                newTile.Position = new Vector2(x * SCALE + TILE_OFFSET_X, y * SCALE + TILE_OFFSET_Y);
                 newTile.Connect("TileSelected", GetParent(), "OnTileSelected");
                 AddChild(newTile);
 
@@ -71,16 +73,16 @@ public class Board : Node2D
 
     public Coordinate FromPosition(Vector2 position)
     {
-        float x = (position.x - POSITION_OFFSET) / SCALE;
-        float y = -((position.y - POSITION_OFFSET) / SCALE - GRID_ROWS + 1);
+        float x = (position.x - POSITION_OFFSET_X) / SCALE;
+        float y = -((position.y - POSITION_OFFSET_X) / SCALE - GRID_ROWS + 1);
 
         return new Coordinate { x_coord = (int)x, y_coord = (int)y };
     }
 
     public Vector2 ToPosition(Coordinate location)
     {
-        int x = location.x_coord * SCALE + POSITION_OFFSET;
-        int y = (-location.y_coord + GRID_ROWS - 1) * SCALE + POSITION_OFFSET;
+        int x = location.x_coord * SCALE + POSITION_OFFSET_X;
+        int y = (-location.y_coord + GRID_ROWS - 1) * SCALE + POSITION_OFFSET_Y;
 
         return new Vector2(x, y);
     }
@@ -98,8 +100,8 @@ public class Board : Node2D
 
     public Coordinate FromTilePosition(Vector2 position)
     {
-        int x = (int)(position.x - TILE_OFFSET) / SCALE;
-        int y = (int)(position.y - TILE_OFFSET) / SCALE;
+        int x = (int)(position.x - TILE_OFFSET_X) / SCALE;
+        int y = (int)(position.y - TILE_OFFSET_Y) / SCALE;
 
         return new Coordinate { x_coord = x, y_coord = y };
     }
